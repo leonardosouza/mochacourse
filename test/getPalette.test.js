7b6a9e748eb87a822459becb36310362dedd4446
Leonardo Souza <leonardodisouza@gmail.com>
2014-11-23 21:01:24 -0200

    after and before hooks

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,16 +1,36 @@
 var getPalette = require('../lib/getPalette');
 var assert = require('assert');
+var fs = require('fs');
+
+var configFile = process.cwd() + "/config.json";
+
+function writeConfig(config, callback) {
+    console.log("===>", config);
+    fs.writeFile(configFile, JSON.stringify(config), callback);
+}
+
 
 describe('getPalette', function () {
-    it('should throw an error if result is not array', function() {
-        function fetch() { 
-            return 'not array';
-        }
+    var config = {};
 
-        // dependency injection
-        assert.throws(function() {
-            getPalette(fetch);
-        }, /is not an array/);
+    before(function(done) {
+        fs.readFile(configFile, function(err, contents) {
+            config = JSON.parse(contents.toString());
+            done();
+        });
+    });
+
+    afterEach(function(done) {
+        writeConfig(config, done);
+    });
+
+
+
+    it('should throw an error if result is not array', function(done) {
+        writeConfig({ palette: "string" }, function(err) {
+            assert.throws(getPalette, /is not an array/);
+            done();
+        });
     });
 
     it('should return an array with 3 items', function () {
