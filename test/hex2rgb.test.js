e0a2d77414c98d4f1b62b2725104e081cc1eb9fe
Leonardo Souza <leonardodisouza@gmail.com>
2014-11-23 20:05:06 -0200

    asynchronous testing

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -2,16 +2,26 @@ var hex2rgb = require('../lib/hex2rgb');
 var assert = require('assert');
 
 describe('hex2rgb', function () {
-    it('should throw an error if result is not a hex code', function() {
+    it('should return an error if result is not a hex code', function(done) {
         
-        assert.throws(function () {
-            hex2rgb("blue")
-        }, /Invalid hexadecimal string/);
+        hex2rgb('blue', function(error, result) {
+            assert(error);
+            done();
+        });
     });
 
-    it('should return a correctly converted rgb value', function() {
-        var rgb = hex2rgb("#fff");
+    it('should return a correctly converted rgb value', function(done) {
+        
 
-        assert.deepEqual(rgb, [255, 255, 255]);
+        hex2rgb("#fff", function(error, result) {
+            assert.strictEqual(error, null);
+            assert.deepEqual(result, [255, 255, 255]);
+            done();
+        });
+        
     })
 });
+
+
+
+
